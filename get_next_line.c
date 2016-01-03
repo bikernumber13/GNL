218438aa1960043cf91f21a9c93a8578aa0b3ce7
Michael BOUHIER <mbouhier@e1r7p3.42.fr>
2016-01-03 11:13:04 +0100

    perfect gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,46 +6,52 @@
 /*   By: mbouhier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/31 18:15:51 by mbouhier          #+#    #+#             */
-/*   Updated: 2016/01/02 13:33:10 by mbouhier         ###   ########.fr       */
+/*   Updated: 2016/01/02 20:47:08 by mbouhier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_end_line(char **line, char **tmp, int const fd, char *tmp2)
+static int	ft_end_line(char *tmp2, char *tmp)
 {
-	*line = ft_strsub(tmp[fd], 0, ft_strchr(tmp[fd], '\n') - tmp[fd]);
-	tmp2 = tmp[fd];
-	tmp[fd] = ft_strsub(tmp[fd], ft_strchr(tmp[fd], '\n') - tmp[fd] + 1,
-		ft_strlen(ft_strchr(tmp[fd], '\n')));
-	free(tmp2);
+	if (!tmp2)
+		return (0);
+	*tmp2 = '\0';
+	tmp2++;
+	ft_strcpy(tmp, tmp2);
+	ft_memset(tmp2, 0, ft_strlen(tmp2));
+	return (1);
 }
 
-int		get_next_line(int const fd, char **line)
+static char	*ft_strjoin_free(char **line, char *buf)
+{
+	char *tmp;
+
+	tmp = *line;
+	*line = ft_strjoin(*line, buf);
+	free(tmp);
+	return (*line);
+}
+
+int			get_next_line(int const fd, char **line)
 {
 	int			ret;
-	char		buf[BUFF_SIZE];
+	char		buf[BUFF_SIZE + 1];
 	char		*tmp2;
-	static char *tmp[1024];
+	static char tmp[1024][BUFF_SIZE];
 
-	if (!line || fd < 0 || BUFF_SIZE < 1 || (!(tmp[fd] = !tmp[fd] ?
-			ft_strnew(1) : tmp[fd])))
+	if (!line || fd < 0 || BUFF_SIZE < 1)
 		return (-1);
-	while (!ft_strchr(tmp[fd], '\n') && (ret = read(fd, buf, BUFF_SIZE)) > 0)
+	if (!(*line = *tmp[fd] ? ft_strdup(tmp[fd]) : ft_strnew(1)))
+		return (-1);
+	while (!(tmp2 = ft_strchr(*line, '\n')) &&
+		(ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		tmp2 = tmp[fd];
-		tmp[fd] = ft_strjoin(tmp[fd], buf);
-		free(tmp2);
+		if (!(*line = ft_strjoin_free(line, buf)))
+			return (-1);
 	}
-	if (!ft_strchr(tmp[fd], '\n'))
-	{
-		*line = ft_strdup(tmp[fd]);
-		tmp[fd] = NULL;
-	}
-	else
-		ft_end_line(line, tmp, fd, tmp2);
-	if (ret == -1)
-		return (-1);
-	return (!ret && !tmp[fd] && !ft_strlen(*line) ? 0 : 1);
+	if (ft_end_line(tmp2, tmp[fd]) == 1 || ret > 0)
+		return (1);
+	return (ret == 0 ? 0 : -1);
 }
